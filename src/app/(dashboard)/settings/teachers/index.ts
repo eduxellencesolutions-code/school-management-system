5d329df5f7e540098cd8b49418783fc9e50b5263
eduxellencesolutions-code <eduxellencesolutions@gmail.com>
2026-07-04 19:01:21 +0100

    Create index.ts

---
+++ src/app/(dashboard)/settings/teachers/index.ts
@@ -0,0 +1,9 @@
+// Re-export all actions from a single file
+export {
+  getTeachers,
+  assignTeacher,
+  removeAssignment,
+  uploadTeachers,
+  downloadTeacherTemplate,
+  deleteTeacher
+} from './actions'
